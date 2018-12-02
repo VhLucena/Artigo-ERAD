dbbb09a2ef27296dd1889b9499fcd0a3b2ab0247
VhLucena <v-lucena@outlook.com>
2018-12-02 17:19:42 -0200

    omp.h incluido. Utilizando o contador de tempo do omp

--- algoritmo/src/main.cpp
+++ algoritmo/src/main.cpp
@@ -5,7 +5,8 @@
 #include <cmath>
 #include <map>
 #include <algorithm>
-#include <glib.h>
+#include <omp.h>
+//#include <glib.h>
 
 using namespace std;
 
@@ -13,170 +14,170 @@ const int NOISE = -2;
 const int NOT_CLASSIFIED = -1;
 
 class Point {
-public:
-    double x, y;
-    int ptsCnt, cluster;
-    double getDis(const Point & ot) {
-        return sqrt((x-ot.x)*(x-ot.x)+(y-ot.y)*(y-ot.y));
-    }
+    public:
+        double x, y;
+        int ptsCnt, cluster;
+        double getDis(const Point & ot) {
+            return sqrt((x-ot.x)*(x-ot.x)+(y-ot.y)*(y-ot.y));
+        }
 };
 
 class DBCAN {
-public:
-    int n, minPts;
-    double eps;
-    vector<Point> points;
-    int size;
-    vector<vector<int> > adjPoints;
-    vector<bool> visited;
-    vector<vector<int> > cluster;
-    int clusterIdx;
-    
-    DBCAN(int n, double eps, int minPts, vector<Point> points) {
-        this->n = n;
-        this->eps = eps;
-        this->minPts = minPts;
-        this->points = points;
-        this->size = (int)points.size();
-        adjPoints.resize(size);
-        this->clusterIdx=-1;
-    }
-    void runSequential () {
-        checkNearPoints();
+    public:
+        int n, minPts;
+        double eps;
+        vector<Point> points;
+        int size;
+        vector<vector<int> > adjPoints;
+        vector<bool> visited;
+        vector<vector<int> > cluster;
+        int clusterIdx;
         
-        for(int i=0;i<size;i++) {
-            if(points[i].cluster != NOT_CLASSIFIED) continue;
+        DBCAN(int n, double eps, int minPts, vector<Point> points) {
+            this->n = n;
+            this->eps = eps;
+            this->minPts = minPts;
+            this->points = points;
+            this->size = (int)points.size();
+            adjPoints.resize(size);
+            this->clusterIdx=-1;
+        }
+        void runSequential () {
+            checkNearPoints();
             
-            if(isCoreObject(i)) {
-                dfs(i, ++clusterIdx);
-            } else {
-                points[i].cluster = NOISE;
+            for(int i=0;i<size;i++) {
+                if(points[i].cluster != NOT_CLASSIFIED) continue;
+                
+                if(isCoreObject(i)) {
+                    dfs(i, ++clusterIdx);
+                } else {
+                    points[i].cluster = NOISE;
+                }
             }
-        }
-        
-        cluster.resize(clusterIdx+1);
-        for(int i=0;i<size;i++) {
-            if(points[i].cluster != NOISE) {
-                cluster[points[i].cluster].push_back(i);
-            }
-        }
-    }
-
-    void runParallel () {
-        checkNearPoints();
-        
-        #pragma omp parallel for
-        for(int i=0;i<size;i++) {
-            if(points[i].cluster != NOT_CLASSIFIED) continue;
             
-            if(isCoreObject(i)) {
-                dfs(i, ++clusterIdx);
-            } else {
-                points[i].cluster = NOISE;
-            }
-        }
-        
-        cluster.resize(clusterIdx+1);
-        for(int i=0;i<size;i++) {
-            if(points[i].cluster != NOISE) {
-                cluster[points[i].cluster].push_back(i);
-            }
-        }
-    }
-    
-    void dfs (int now, int c) {
-        points[now].cluster = c;
-        if(!isCoreObject(now)) return;
-        
-        for(auto&next:adjPoints[now]) {
-            if(points[next].cluster != NOT_CLASSIFIED) continue;
-            dfs(next, c);
-        }
-    }
-    
-    void checkNearPoints() {
-        for(int i=0;i<size;i++) {
-            for(int j=0;j<size;j++) {
-                if(i==j) continue;
-                if(points[i].getDis(points[j]) <= eps) {
-                    points[i].ptsCnt++;
-                    adjPoints[i].push_back(j);
+            cluster.resize(clusterIdx+1);
+            for(int i=0;i<size;i++) {
+                if(points[i].cluster != NOISE) {
+                    cluster[points[i].cluster].push_back(i);
                 }
             }
         }
-    }
-    // is idx'th point core object?
-    bool isCoreObject(int idx) {
-        return points[idx].ptsCnt >= minPts;
-    }
-    
-    vector<vector<int> > getCluster() {
-        return cluster;
-    }
+
+        void runParallel () {
+            checkNearPoints();
+            
+            #pragma omp parallel for
+            for(int i=0;i<size;i++) {
+                if(points[i].cluster != NOT_CLASSIFIED) continue;
+                
+                if(isCoreObject(i)) {
+                    dfs(i, ++clusterIdx);
+                } else {
+                    points[i].cluster = NOISE;
+                }
+            }
+            
+            cluster.resize(clusterIdx+1);
+            for(int i=0;i<size;i++) {
+                if(points[i].cluster != NOISE) {
+                    cluster[points[i].cluster].push_back(i);
+                }
+            }
+        }
+        
+        void dfs (int now, int c) {
+            points[now].cluster = c;
+            if(!isCoreObject(now)) return;
+            
+            for(auto&next:adjPoints[now]) {
+                if(points[next].cluster != NOT_CLASSIFIED) continue;
+                dfs(next, c);
+            }
+        }
+        
+        void checkNearPoints() {
+            for(int i=0;i<size;i++) {
+                for(int j=0;j<size;j++) {
+                    if(i==j) continue;
+                    if(points[i].getDis(points[j]) <= eps) {
+                        points[i].ptsCnt++;
+                        adjPoints[i].push_back(j);
+                    }
+                }
+            }
+        }
+        // is idx'th point core object?
+        bool isCoreObject(int idx) {
+            return points[idx].ptsCnt >= minPts;
+        }
+        
+        vector<vector<int> > getCluster() {
+            return cluster;
+        }
 };
 
 class InputReader {
-private:
-    ifstream fin;
-    vector<Point> points;
-public:
-    InputReader(string filename) {
-        fin.open(filename);
-        if(!fin) {
-            cout << filename << " file could not be opened\n";
-            exit(0);
+    private:
+        ifstream fin;
+        vector<Point> points;
+    public:
+        InputReader(string filename) {
+            fin.open(filename);
+            if(!fin) {
+                cout << filename << " file could not be opened\n";
+                exit(0);
+            }
+            parse();
         }
-        parse();
-    }
-    void parse() {
-        int idx;
-        double x, y;
-        while(!fin.eof()) {
-            fin >> idx >> x >> y;
-            points.push_back({x,y,0, NOT_CLASSIFIED});
+        void parse() {
+            int idx;
+            double x, y;
+            while(!fin.eof()) {
+                fin >> idx >> x >> y;
+                points.push_back({x,y,0, NOT_CLASSIFIED});
+            }
+            points.pop_back();
         }
-        points.pop_back();
-    }
-    vector<Point> getPoints() {
-        return points;
-    }
+        vector<Point> getPoints() {
+            return points;
+        }
 };
 
 class OutputPrinter {
-private:
-    ofstream fout;
-    vector<vector<int> > cluster;
-    string filename;
-    int n;
-public:
-    OutputPrinter(int n, string filename, vector<vector<int> > cluster) {
-        this->n = n;
-        this->cluster = cluster;
-        
-        // remove ".txt" from filename
-        if(filename.size()<4){
-            cout << filename << "input file name's format is wrong\n";
-            exit(0);
-        }
-        for(int i=0;i<4;i++) filename.pop_back();
-        this->filename = filename;
-        
-        // sort by size decending order
-        sort(cluster.begin(), cluster.end(), [&](const vector<int> i, const vector<int> j) {
-            return (int)i.size() > (int)j.size();
-        });
-    }
-    void print() {
-        for(int i=0;i<n;i++) {
-            fout.open(filename+"_cluster_"+to_string(i)+".txt");
+    private:
+        ofstream fout;
+        vector<vector<int> > cluster;
+        string filename;
+        int n;
+    public:
+        OutputPrinter(int n, string filename, vector<vector<int> > cluster) {
+            this->n = n;
+            this->cluster = cluster;
             
-            for(int j=0;j<cluster[i].size();j++) {
-                fout << cluster[i][j] << endl;
+            // remove ".txt" from filename
+            if(filename.size()<4){
+                cout << filename << "input file name's format is wrong\n";
+                exit(0);
             }
+            for(int i=0;i<4;i++) filename.pop_back();
+            this->filename = filename;
             
-            fout.close();
+            // sort by size decending order
+            sort(cluster.begin(), cluster.end(), [&](const vector<int> i, const vector<int> j) {
+                return (int)i.size() > (int)j.size();
+            });
         }
-    }
+        void print() {
+            for(int i=0;i<n;i++) {
+                fout.open(filename+"_cluster_"+to_string(i)+".txt");
+                
+                for(int j=0;j<cluster[i].size();j++) {
+                    fout << cluster[i][j] << endl;
+                }
+                
+                fout.close();
+            }
+        }
 };
 
 int main(int argc, const char * argv[]) {
@@ -189,6 +190,8 @@ int main(int argc, const char * argv[]) {
     string n(argv[2]);
     string eps(argv[3]);
     string minPts(argv[4]);
+
+    double start_time, elapsed_time;
     
     InputReader inputReader(inputFileName);
     
@@ -197,14 +200,11 @@ int main(int argc, const char * argv[]) {
 // ----------------------------------------------------------------------- //
 //                          SEQUENCIAL
 // ----------------------------------------------------------------------- //
-	GTimer* timer = g_timer_new();
+	start_time = omp_get_wtime();
     dbScan.runSequential();
-	g_timer_stop(timer);
+	elapsed_time = omp_get_wtime() - start_time;
 
-	gulong micro;
-	double elapsed = g_timer_elapsed(timer, &micro);
-	
-	printf("Sequencial: %lf seg \n\n", (elapsed*1));
+	printf("Sequencial: %lf seconds \n\n", (elapsed_time*1));
 // ----------------------------------------------------------------------- //
 
 
@@ -212,14 +212,11 @@ int main(int argc, const char * argv[]) {
 // ----------------------------------------------------------------------- //
 //                          PARALELO
 // ----------------------------------------------------------------------- //
-	GTimer* timer_parallel = g_timer_new();
+	start_time = omp_get_wtime();
     dbScan.runParallel();
-	g_timer_stop(timer_parallel);
+	elapsed_time = omp_get_wtime() - start_time;
 
-	gulong micro_parallel;
-	double elapsed_parallel = g_timer_elapsed(timer_parallel, &micro_parallel);
-	
-	printf("Paralelo: %lf seg \n\n", (elapsed_parallel*1));
+	printf("Paralelo: %lf seconds \n\n", (elapsed_time*1));
 // ----------------------------------------------------------------------- //
     
     //OutputPrinter outputPrinter(stoi(n), inputFileName, dbScan.getCluster());
